eef1c1e67125e961db8944e30bc399700c49819d
Rob Hesketh <rhesketh.uk@gmail.com>
2019-08-12 22:57:48 +0100

    Make email signin case-agnostic

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
+  before_create :force_email_lowercase
+
+  private
+
+  def force_email_lowercase
+    self.email = email.downcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'forces email to lowercase on creation' do
+    user = described_class.create!(email: 'Test@example.com', password: 'abc', password_confirmation: 'abc')
+    expect(user.reload.email).to eq 'test@example.com'
+  end
 end
