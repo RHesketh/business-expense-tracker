10320b00e19f789fed0fc4d8b54896a406104c4e
Rob Hesketh <rhesketh.uk@gmail.com>
2019-08-12 22:24:25 +0100

    Added the ability for users to log in

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@
 
 Rails.application.routes.draw do
   root to: 'welcome#index'
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'sign_in', to: 'sessions#new', as: 'sign_in'
+  get 'sign_out', to: 'sessions#destroy', as: 'sign_out'
 end
--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -11,7 +11,7 @@ feature 'Visiting the home page' do
     scenario 'User is offered a link to sign in' do
       visit root_path
       click_link 'sign in'
-      # expect(page.current_path).to eq '/sign_in' #change to sign_in_path
+      expect(page.current_path).to eq sign_in_path
     end
   end
 end
